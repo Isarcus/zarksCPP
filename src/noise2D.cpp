fc0db127cafc3bd6babb7a01a34f538a5504d6cd
Isaac <iabrooks@mtu.edu>
2021-08-28 12:52:31 -0400

    Updated Simplex, Perlin, and VecT hash

--- src/noise2D.cpp
+++ src/noise2D.cpp
@@ -11,15 +11,15 @@
 namespace std
 {
 	// Necessary in order to use Vec as a key in an std::unordered_map
-	template <>
-	struct hash<zmath::Vec>
+	template <typename T>
+	struct hash<zmath::VecT<T>>
 	{
-		size_t operator()(const zmath::Vec& k) const
+		size_t operator()(const zmath::VecT<T>& k) const
 		{
-			const size_t multBy = 73; // 31 
+			static constexpr size_t multBy = 73; // 31 
 			size_t res = 17;
-			res = res * multBy + hash<double>()(k.X);
-			res = res * multBy + hash<double>()(k.Y);
+			res = res * multBy + hash<T>()(k.X);
+			res = res * multBy + hash<T>()(k.Y);
 			return res;
 		}
 	};
@@ -57,25 +57,20 @@ namespace zmath
 		seed = std::chrono::system_clock::now().time_since_epoch().count();
 	}
 
-	// Note: runs more than 2x faster than the Golang equivalent I made first. Woohoo!
-	// This might actually be my fastest implementation yet, and it's not even fully optimized
-	Map Simplex(NoiseConfig cfg)
+	Map Simplex(const NoiseConfig& cfg)
 	{
-		// Refer to https://github.com/Isarcus/zarks/blob/main/zmath/noise/simplex.go for original code.
-
-		// Constants
-		constexpr static const double F2D = 0.3660254037844386467637231707529361834714026;
-		constexpr static const double G2D = 0.2113248654051871177454256097490212721761991;
+		// Helpful constants
+		static constexpr double F2D = 0.3660254037844386467637231707529361834714026;
+		static constexpr double G2D = 0.2113248654051871177454256097490212721761991;
 		auto skew =   [](Vec pt) { return Vec(pt.X + pt.Sum() * F2D, pt.Y + pt.Sum() * F2D); };
 		auto unskew = [](Vec pt) { return Vec(pt.X - pt.Sum() * G2D, pt.Y - pt.Sum() * G2D); };
-		double r2 = cfg.r * cfg.r;
+		const double r2 = cfg.r * cfg.r;
 
 		// RNG
 		std::default_random_engine eng(cfg.seed);
 		std::uniform_real_distribution<double> angleRNG(0, 2.0*PI);
 
 		// Map setup
-		cfg.bounds = cfg.bounds.Floor();
 		Map map(cfg.bounds);
 
 		std::cout << "Creating new Simplex Map:\n";
@@ -86,62 +81,77 @@ namespace zmath
 		// Dive in
 		for (int oct = 0; oct < cfg.octaves; oct++)
 		{
+			// Calculate the influence of this octave on the overall noise map.
+			// Influence most commonly decreases by a factor of 2 each octave.
 			double octInfluence = std::pow(cfg.octDecrease, oct);
+
+			// Calculate vector with which to scale coordinates ths octave.
 			Vec scaleVec = (Vec(1.0, 1.0) / cfg.boxSize) / std::pow(0.5, oct);
 
-			std::unordered_map<Vec, Vec> hash;
+			// Create hashmap from coordinates to unit vectors to reuse randomly generated angles
+			std::unordered_map<VecInt, Vec> hash;
 
+			// Iterate through the whole map
 			for (int x = 0; x < cfg.bounds.X; x++)
 			{
 				for (int y = 0; y < cfg.bounds.Y; y++)
 				{
+					// Compute input coordinate
 					Vec ipt = scaleVec * Vec(x, y);
+					// Compute skewed coordinate
 					Vec skewed = skew(ipt);
-					Vec itl = skewed - skewed.Floor(); // internal simplex coordinate
+					// Compute internal simplex coordinate
+					Vec itl = skewed - skewed.Floor();
 
-					Vec corners[3];
+					// Corners of this simplex
+					VecInt corners[3];
+					// Angle vectors of this simplex's corners
 					Vec vectors[3];
 
+					// Base corner is just lowest point in this simplex
 					corners[0] = skewed.Floor();
-					corners[1] = (itl.X > itl.Y) ? corners[0] + Vec(1, 0) : corners[0] + Vec(0, 1);
+					// "Middle" corner depends on internal X and Y
+					corners[1] = (itl.X > itl.Y) ? corners[0] + VecInt(1, 0) : corners[0] + VecInt(0, 1);
+					// High corner is just highers point in this simplex
 					corners[2] = corners[0] + Vec(1, 1);
 
 					for (int i = 0; i < 3; i++)
 					{
-						if (hash.find(corners[i]) == hash.end())
+						if (hash.count(corners[i]))
 						{
-							// if the current key does not exist, generate a new key-value pair
-							double angle = angleRNG(eng);
-							vectors[i] = Vec(std::cos(angle), std::sin(angle));
-
-							hash.emplace(corners[i], vectors[i]);
+							// Use the already assigned angle vector if it exists
+							vectors[i] = hash.at(corners[i]);
 						}
 						else
 						{
-							// otherwise just use the existing value
-							vectors[i] = hash.at(corners[i]);
+							// Otherwise, generate and assign a new one
+							double angle = angleRNG(eng);
+							vectors[i] = Vec(std::cos(angle), std::sin(angle));
+							hash.emplace(corners[i], vectors[i]);
 						}
 					}
 
-					// Final summation
+					// Perform final summation for this coordinate
 					double Z = 0;
 					for (int i = 0; i < 3; i++)
 					{
 						Vec displacement = ipt - unskew(corners[i]);
 						double distance = displacement.LNorm(cfg.lNorm); // Distance formula
 
-						// change 4.0 value for cool effects!
 						double influence = std::pow(std::max(0.0, r2 - distance * distance), cfg.rMinus);
 						Z += influence * displacement.Dot(vectors[i]);
 					}
 
+					// Add this coordinate to the map, weighted appropriately
 					map.At(x, y) += Z * octInfluence;
 				}
 			} 
 
-			std::cout << " -> Octave " << oct+1 << " Finished.\n";
+			// Notify octave completion
+			std::cout << " -> Octave " << oct + 1 << " Finished.\n";
 		}
 
+		// Perform final normalization, if applicable
 		if (cfg.normalize) map.Interpolate(0, 1);
 
 		return map;
@@ -150,7 +160,7 @@ namespace zmath
 	// Note to self: it seems like hash maps are a solid way to go for storing RNG noise vectors. This isn't as fast,
 	// however, as the Golang implementation at https://github.com/Isarcus/zarks/blob/main/zmath/noise/perlin.go because
 	// that one goes box-by-box rather than iterating across the image column-by-column. Still, this one is respectable!
-	Map Perlin(NoiseConfig cfg)
+	Map Perlin(const NoiseConfig& cfg)
 	{
 		// RNG
 		std::default_random_engine eng(cfg.seed);
@@ -167,58 +177,70 @@ namespace zmath
 		// Beep beep so let's ride
 		for (int oct = 0; oct < cfg.octaves; oct++)
 		{
+			std::unordered_map<VecInt, Vec> hash;
 			double octInfluence = std::pow(cfg.octDecrease, oct);
-			Vec scaleVec = (Vec(1.0, 1.0) / cfg.boxSize) / std::pow(0.5, oct);
+			Vec octaveBoxSize = cfg.boxSize * octInfluence;
 
-			std::unordered_map<Vec, Vec> hash;
-
-			for (int x = 0; x < cfg.bounds.X; x++)
+			// Box minimum x and y
+			for (double bx = 0, bnx = 0; bx < cfg.bounds.X; bx += octaveBoxSize.X, bnx++)
 			{
-				for (int y = 0; y < cfg.bounds.Y; y++)
+				for (double by = 0, bny = 0; by < cfg.bounds.Y; by += octaveBoxSize.Y, bny++)
 				{
-					Vec coord = scaleVec * Vec(x, y);
-					Vec base = coord.Floor();
-					Vec itl = coord - base;
-					
-					double dots[2][2]; // Making El Truco proud
-					for (int vx = 0; vx <= 1; vx++)
+					// Determine vectors of this box's corners
+					VecInt base = Vec(bx, by).Floor();
+					Vec corners[2][2];
+					for (int cx = 0; cx <= 1; cx++)
 					{
-						for (int vy = 0; vy <= 1; vy++)
+						for (int cy = 0; cy <= 1; cy++)
 						{
-							// declaring vector here (instead of *always* using the hash) slightly improves
-							// performance in high-octave passes
-							Vec vector;
-
 							// Randomly generate new vectors if not already present
-							Vec test = base + Vec(vx, vy);
-							if (hash.find(test) == hash.end())
+							VecInt test = VecInt(bnx + cx, bny + cy);
+							if (hash.count(test))
+							{
+								corners[cx][cy] = hash.at(test);
+							}
+							else
 							{
 								double angle = angleRNG(eng);
 								Vec value = Vec(std::cos(angle), std::sin(angle));
 								hash.emplace(test, value);
 
-								vector = value;
+								corners[cx][cy] = value;
 							}
-							else
-							{
-								vector = hash.at(test);
-							}
-
-							// Get dot product
-							dots[vx][vy] = (itl - Vec(vx, vy)).Dot(vector);
 						}
 					}
 
-					double y0, y1, Z;
-					y0 = interp5(dots[0][0], dots[1][0], itl.X);
-					y1 = interp5(dots[0][1], dots[1][1], itl.X);
-					Z = interp5(y0, y1, itl.Y);
+					// Loop through this box and sum to map
+					VecInt thisBoxSize = (Vec(bx, by) + octaveBoxSize).Floor() - Vec(bx, by).Floor();
+					double dots[2][2];
+					for (int ix = 0; ix < thisBoxSize.X; ix++)
+					{
+						if (base.X + ix > cfg.bounds.X) continue;
 
-					map.At(x, y) += Z * octInfluence;
+						for (int iy = 0; iy < thisBoxSize.Y; iy++)
+						{
+							if (base.Y + iy > cfg.bounds.Y) continue;
+
+							Vec itl = Vec(ix, iy) / Vec(thisBoxSize);
+							VecInt absolute = base + VecInt(ix, iy);
+
+							dots[0][0] = (itl - Vec(0, 0)).Dot(corners[0][0]);
+							dots[0][1] = (itl - Vec(0, 1)).Dot(corners[0][1]);
+							dots[1][0] = (itl - Vec(1, 0)).Dot(corners[1][0]);
+							dots[1][1] = (itl - Vec(1, 1)).Dot(corners[1][1]);
+
+							double y0, y1, Z;
+							y0 = interp5(dots[0][0], dots[1][0], itl.X);
+							y1 = interp5(dots[0][1], dots[1][1], itl.X);
+							Z = interp5(y0, y1, itl.Y);
+
+							map.At(absolute) += Z * octInfluence;
+						}
+					}
 				}
 			}
 
-			std::cout << " -> Octave " << oct+1 << " finished.\n";
+			std::cout << " -> Octave " << oct + 1 << " finished.\n";
 		}
 
 		if (cfg.normalize) map.Interpolate(0, 1);
@@ -226,7 +248,7 @@ namespace zmath
 		return map;
 	}
 
-	Map Worley(NoiseConfig cfg)
+	Map Worley(const NoiseConfig& cfg)
 	{
 		// this coordList works for simpler algorithms that use fewer than ~5 points
 		std::vector<Vec> coordList;
@@ -317,7 +339,7 @@ namespace zmath
 		return map;
 	}
 
-	Map WorleyPlex(NoiseConfig cfg, Map& baseMap)
+	Map WorleyPlex(const NoiseConfig& cfg, const Map& baseMap)
 	{
 		if (cfg.bounds != baseMap.Bounds())
 		{
