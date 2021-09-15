adb16cb7563da515879488ce1feb491738b9cfa9
Isaac Brooks <iabrooks@mtu.edu>
2021-09-15 14:06:49 -0400

    Added NoiseHash class

---
+++ include/zarks/noise/NoiseHash.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <zarks/internal/noise_internals.h>
+
+#include <random>
+#include <unordered_map>
+
+namespace zmath
+{
+    class NoiseHash
+    {
+    public:
+        NoiseHash(uint64_t seed = 0);
+
+        bool Exists(VecInt key) const;
+        Vec& Create(VecInt key, Vec val);
+        Vec& Create(VecInt key);
+
+        Vec& operator[](VecInt key);
+
+        Vec& At(VecInt key);
+        const Vec& At(VecInt key) const;
+
+    private:
+        std::unordered_map<VecInt, Vec> hash;
+        std::default_random_engine eng;
+        std::uniform_real_distribution<double> angleRNG;
+    };
+} // namespace zmath
