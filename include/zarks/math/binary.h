a0ab9b836fee40fb060a10acd49981e132b93578
Isaac Brooks <iabrooks@mtu.edu>
2021-09-16 23:40:45 -0400

    Added WriteBuf & ReadBuf, comments in binary.h

--- include/zarks/math/binary.h
+++ include/zarks/math/binary.h
@@ -16,7 +16,23 @@ namespace zmath
 	static const int __one__ = 1;
 	static const Endian CPU_ENDIANNESS = (1 == *(char*)(&__one__)) ? Endian::Little : Endian::Big;
 
-    template <typename var_T, typename buf_T,
+	// Returns the original value of the pointer when passed in,
+	// after incrementing the pointer by 'bytesForward' bytes.
+	template <typename T, typename std::enable_if<sizeof(T) == 1>::type* = nullptr>
+	T* AdvancePtr(T*& ptr, int bytesForward)
+	{
+		ptr += bytesForward;
+		return ptr - bytesForward;
+	}
+
+	// Serialize a value to a binary buffer.
+	// @param buf the buffer to write to. Must have at least
+	//        sizeof(var_T) free space, or using this function
+	//        may result in undefined behavior.
+	// @param val the variable to serialize.
+	// @param byteOrder the desired endianness with which to
+	//        serialize `val`.
+	template <typename var_T, typename buf_T,
               typename std::enable_if<
                  std::is_same<buf_T, char>::value ||
                  std::is_same<buf_T, unsigned char>::value
@@ -30,6 +46,11 @@ namespace zmath
 		}
 	}
 
+	// Deserialize a value from a binary buffer.
+	// @param buf the buffer to deserialize from.
+	// @param bufByteOrder the endianness with which
+	//        the buffer was originally written to.
+	// @return A deserialized variable of type var_T.
 	template <typename var_T, typename buf_T = char,
               typename std::enable_if<
                  std::is_same<buf_T, char>::value ||
@@ -46,12 +67,39 @@ namespace zmath
 		return var;
 	}
 
-	// Returns the original value of the pointer when passed in
-	// after incrementing the pointer by 'bytesForward'.
-	template <typename T, typename std::enable_if<sizeof(T) == 1>::type* = nullptr>
-	T* AdvancePtr(T*& ptr, int bytesForward)
+	// Serialize a value to a binary buffer, and 
+	//  advance the buffer along.
+	// @param buf the buffer to write to. Before returning,
+	//        this buffer will be advanced by sizeof(var_T)
+	//        bytes.
+	// @param val the value to write to the buffer.
+	// @param bufByteOrder the endianness with which to
+	//        serialize `val`.
+	template <typename var_T, typename buf_T,
+		typename std::enable_if<
+			std::is_same<buf_T, char>::value ||
+			std::is_same<buf_T, unsigned char>::value
+		>::type* = nullptr>
+	void WriteBuf(const buf_T*& buf, var_T val, Endian bufByteOrder)
 	{
-		ptr += bytesForward;
-		return ptr - bytesForward;
+		ToBytes(AdvancePtr(buf, sizeof(var_T)), val, bufByteOrder);
+	}
+
+	// Deserialize a value from a binary buffer, and 
+	//  advance the buffer along.
+	// @param buf the buffer to deserialize from. Before
+	//        returning, this buffer will be advanced by
+	//        sizeof(var_T) bytes.
+	// @param bufByteOrder the endianness with which the
+	//        buffer was originally written to.
+	// @return A deserialized variable of type var_T.
+	template <typename var_T, typename buf_T = char,
+			typename std::enable_if<
+				std::is_same<buf_T, char>::value ||
+				std::is_same<buf_T, unsigned char>::value
+			>::type* = nullptr>
+	var_T ReadBuf(const buf_T*& buf, Endian bufByteOrder)
+	{
+		return FromBytes<var_T>(AdvancePtr(buf, sizeof(var_T)), bufByteOrder);
 	}
 }
