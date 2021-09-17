66d9e145fc3ce26377ca80a86cfd8541c7e34f9e
Isaac Brooks <iabrooks@mtu.edu>
2021-09-17 00:09:35 -0400

    Added GIF.cpp, partially done with GIF reading

--- include/zarks/image/GIF.h
+++ include/zarks/image/GIF.h
@@ -17,6 +17,8 @@ namespace zmath
         // throws std::runtime_error.
         GIF(std::string path);
         // Load a GIF from a binary input stream.
+        GIF(std::istream&& is);
+        // Load a GIF from a binary input stream.
         GIF(std::istream& is);
 
         // Write a GIF to file!
@@ -38,7 +40,7 @@ namespace zmath
         //        If palette.size() < 2, this function throws
         //        std::runtime_error. If palette.size() > 256,
         //        only the first 256 colors will be used.
-        void Save(std::string path, std::vector<RGBA> palette, VecInt bounds = VecInt(0, 0)) const;
+        void Save(std::string path, const std::vector<RGBA>& palette, VecInt bounds = VecInt(0, 0)) const;
 
         // Adds a frame to the GIF.
         // @param img the Image to be added
@@ -65,5 +67,155 @@ namespace zmath
 
     private:
         std::deque<Image> frames;
+
+        static constexpr const char* HEADER_89a = "GIF89a";
+        static constexpr const char* HEADER_87a = "GIF87a";
+
+        static constexpr uint8_t IMAGE_SEPARATOR = 0x2C;
+        static constexpr uint8_t EXTENSION_INTRODUCER = 0x21;
+        static constexpr uint8_t GRAPHICS_CONTROL_LABEL = 0xF9;
+        static constexpr uint8_t APPLICATION_CONTROL_LABEL = 0xFF;
+
+        // Struct for Logical Screen Descriptor bit flags
+        typedef struct LSDFlags
+        {
+            // Zero-initializes all fields
+            LSDFlags();
+            // Requires the fifth byte of the Logical Screen Descriptor
+            LSDFlags(uint8_t flagByte);
+
+            unsigned colorTableSize : 3;
+            unsigned sortFlag : 1;
+            unsigned colorResolution : 3;
+            unsigned globalTableFlag : 1;
+
+            static constexpr uint8_t COLOR_TABLE_SIZE        = 0b00000111;
+            static constexpr uint8_t SORT_FLAG               = 0b00001000;
+            static constexpr uint8_t COLOR_RESOLUTION        = 0b01110000;
+            static constexpr uint8_t GLOBAL_COLOR_TABLE_FLAG = 0b10000000;
+        } LSDFlags;
+
+        // Struct for Image Descriptor bit flags
+        typedef struct IDFlags
+        {
+            // Zero-initializes all fields
+            IDFlags();
+            // Requires the tenth byte of an Image Descriptor
+            IDFlags(uint8_t flagByte);
+
+            unsigned colorTableSize : 3;
+            unsigned __reservedBits : 2;
+            unsigned sortFlag : 1;
+            unsigned interlaceFlag : 1;
+            unsigned localTableFlag : 1;
+
+            static constexpr uint8_t COLOR_TABLE_SIZE       = 0b00000111;
+            static constexpr uint8_t RESERVED_BITS          = 0b00011000;
+            static constexpr uint8_t SORT_FLAG              = 0b00100000;
+            static constexpr uint8_t INTERLACE_FLAG         = 0b01000000;
+            static constexpr uint8_t LOCAL_COLOR_TABLE_FLAG = 0b10000000;
+        } IDFlags;
+        
+        typedef struct ImageDescriptor
+        {
+            // Requires that the stream has *just* read the first byte
+            // of an image descriptor, i.e. IMAGE_SEPARATOR.
+            ImageDescriptor(std::istream& is);
+
+            uint16_t offsetWidth, offsetHeight;
+            uint16_t width, height;
+            IDFlags flags;
+        } ImageDescriptor;
+
+        //                 //
+        // Helpful Methods //
+        //                 //
+
+        // Load a frame from the GIF input stream, or throw EndOfStreamException 
+        //  if there are no more frames to be loaded. If `globalColorTable` is 
+        //  empty and the loaded frame is missing a local color table, then an 
+        //  instance of NoColorTableException will be thrown.
+        // @param is the input stream to read from.
+        // @param globalColorTable the global color table. This should be left
+        //        empty if there is no global color table.
+        // @return An std::pair containing an image and the duration of that
+        //         image. the duration will be 0 if not specified by a graphics
+        //         extension block preceding the loaded image.
+        static std::pair<Image, uint16_t> loadNextFrame(std::istream& is, const std::vector<RGBA>& globalColorTable);
+
+        // Load an image from the GIF input stream, starting immediately after 
+        //  the Image Descriptor bytes
+        // @param is the input stream to read from.
+        // @return the raw LZW-compressed image data of one image frame,
+        //         without the separator byte in between blocks.
+        static std::vector<uint8_t> loadImageData(std::istream& is);
+
+        // Decode raw LZW data into individual LZW codes.
+        // @param data the raw LZW-compressed image data of one image frame
+        // @return a vector of 16-bit LZW codes
+        static std::vector<uint16_t> decompressLZW(const std::vector<uint8_t>& data);
+
+        // Decode a series of LZW image codes given a color table.
+        //  If any LZW codes reference a color beyond the length of the
+        //  color table, then ColorTableOutOfBoundsException will be
+        //  thrown.
+        // @param codes LZW codes
+        // @param colorTable a colorTable
+        // @return an LZW-decoded GIF frame
+        static Image decodeImage(const std::vector<uint16_t>& codes, const std::vector<RGBA>& colorTable);
+
+        // @param is the input stream to read from.
+        // @param numColors this should refer to the total number of colors in
+        //                  the table, i.e. 2^(1 + LSDFlags::colorTableSize).
+        // @return An RGB color table of size `numColors`
+        static std::vector<RGBA> loadColorTable(std::istream& is, uint8_t numColors);
+
+        // @param bitField a value from 0-7; the last three bits of
+        //        the fifth byte of the Logical Screen Descriptor or
+        //        the last three bits of the tenth byte of an Image
+        //        Descriptor (e.g. LSDFlags::colorTableSize and
+        //        IDFlags::colorTableSize).
+        // @return 2^(1 + bitField)
+        static int getColorTableSize(int bitField);
+
     };
+
+    namespace gif
+    {
+        //                    //
+        // Helpful Exceptions //
+        //                    //
+
+        class GifLoadingException : public std::runtime_error
+        {
+        public:
+            GifLoadingException(std::string error);
+            virtual std::string getErrorName() const = 0;
+
+            friend std::ostream& operator<<(std::ostream& os, const GifLoadingException& e); 
+        };
+
+        class EndOfStreamException : public GifLoadingException
+        {
+        public:
+            EndOfStreamException(std::string error);
+            virtual std::string getErrorName() const override;
+        };
+
+        class InterruptedStreamException : public GifLoadingException
+        {
+        public:
+            InterruptedStreamException(std::string error);
+            virtual std::string getErrorName() const override;
+        };
+
+        class ColorTableException : public GifLoadingException
+        {
+        public:
+            ColorTableException(std::string error);
+            virtual std::string getErrorName() const override;
+        };
+
+    } // namespace gif
+    
 } // namespace zmath
---
+++ src/GIF.cpp
@@ -0,0 +1,329 @@
+#include <zarks/image/GIF.h>
+#include <zarks/math/binary.h>
+
+#include <fstream>
+#include <cstring>
+
+namespace zmath
+{
+
+GIF::GIF()
+{}
+
+GIF::GIF(std::string path)
+    : GIF(std::ifstream(path, std::ios_base::binary))
+{}
+
+GIF::GIF(std::istream&& is)
+    : GIF(is)
+{}
+
+GIF::GIF(std::istream& is)
+{
+    // Check validity of stream
+    if (!is)
+    {
+        throw std::runtime_error("Invalid input stream while reading GIF");
+    }
+
+    // Read 6-byte header and make sure GIF is either 89a or 87a format
+    char header[7]{};
+    is.read(header, 6);
+    if (std::strcmp(header, HEADER_87a) &&
+        std::strcmp(header, HEADER_89a))
+    {
+        throw std::runtime_error(std::string("Invalid GIF header: ") + header);
+    }
+    
+    // Read the 7-byte logical screen descriptor
+    char descriptor[7], *ptr = descriptor;
+    is.read(descriptor, 7);
+    __attribute__((unused)) uint16_t canvasWidth = FromBytes<uint16_t>(AdvancePtr(ptr, 2), Endian::Little);
+    __attribute__((unused)) uint16_t canvasHeight = FromBytes<uint16_t>(AdvancePtr(ptr, 2), Endian::Little);
+    LSDFlags flags(*AdvancePtr(ptr, 1));
+    // Ignore the last two bytes of the descriptor. The following link
+    // explains why the 6th and 7th bytes, which represent the background
+    // color index and the pixel aspect ratio, respectively, have no use
+    // in a modern GIF viewer:
+    // http://giflib.sourceforge.net/whatsinagif/bits_and_bytes.html
+
+    // If applicable, load the global color table
+    std::vector<RGBA> globalColorTable;
+    if (flags.globalTableFlag)
+    {
+        globalColorTable = loadColorTable(is, getColorTableSize(flags.colorTableSize));
+    }
+
+    // Process blocks for as long as needed
+    while (true)
+    {
+        try {
+            auto pair = loadNextFrame(is, globalColorTable);
+            frames.push_back(pair.first);
+        } catch (const gif::ColorTableException& ncte) {
+            std::cerr << ncte << "\n";
+        } catch (const gif::InterruptedStreamException& ise) {
+            std::cerr << ise << "\n";
+        } catch (const gif::EndOfStreamException& eose) {
+            std::cout << eose << "\n";
+            break;
+        }
+    }
+}
+
+void GIF::Save(std::string path, uint8_t paletteSize, VecInt bounds) const
+{
+
+}
+
+void GIF::Save(std::string path, const std::vector<RGBA>& palette, VecInt bounds) const
+{
+
+}
+
+void GIF::Add(const Image& img, bool adjustBounds, int idx)
+{
+    if (idx == -1) {
+        frames.push_back(img);
+    } else {
+        auto iter = frames.begin();
+        std::advance(iter, idx);
+        frames.insert(iter, img);
+    }
+}
+
+Image& GIF::At(int frame)
+{
+    if (frame == -1)
+        return frames.back();
+    else
+        return frames.at(frame);
+}
+
+const Image& GIF::At(int frame) const
+{
+    if (frame == -1)
+        return frames.back();
+    else
+        return frames.at(frame);
+}
+
+int GIF::Size() const
+{
+    return frames.size();
+}
+
+VecInt GIF::Bounds() const
+{
+    if (frames.size())
+        return frames[0].Bounds();
+    else
+        return VecInt(0, 0);
+}
+
+//                //
+// Struct Methods //
+//                //
+
+GIF::LSDFlags::LSDFlags()
+    : colorTableSize(0)
+    , sortFlag(0)
+    , colorResolution(0)
+    , globalTableFlag(0)
+{}
+
+GIF::LSDFlags::LSDFlags(uint8_t flagByte)
+    : colorTableSize  ( flagByte & COLOR_TABLE_SIZE)
+    , sortFlag        ((flagByte & SORT_FLAG)              >> 3)
+    , colorResolution ((flagByte & COLOR_RESOLUTION)       >> 4)
+    , globalTableFlag ((flagByte & GLOBAL_COLOR_TABLE_FLAG >> 7))
+{}
+
+GIF::IDFlags::IDFlags()
+    : colorTableSize(0)
+    , __reservedBits(0)
+    , sortFlag(0)
+    , interlaceFlag(0)
+    , localTableFlag(0)
+{}
+
+GIF::IDFlags::IDFlags(uint8_t flagByte)
+    : colorTableSize ( flagByte & COLOR_TABLE_SIZE)
+    , __reservedBits ((flagByte & RESERVED_BITS)          >> 3)
+    , sortFlag       ((flagByte & SORT_FLAG)              >> 5)
+    , interlaceFlag  ((flagByte & INTERLACE_FLAG)         >> 6)
+    , localTableFlag ((flagByte & LOCAL_COLOR_TABLE_FLAG) >> 7)
+{}
+
+GIF::ImageDescriptor::ImageDescriptor(std::istream& is)
+{
+    uint8_t buf[9];
+    const uint8_t *ptr = buf;
+
+    is.read((char*)buf, 9);
+
+    offsetWidth = ReadBuf<uint16_t>(ptr, Endian::Little);
+    offsetHeight = ReadBuf<uint16_t>(ptr, Endian::Little);
+    width = ReadBuf<uint16_t>(ptr, Endian::Little);
+    height = ReadBuf<uint16_t>(ptr, Endian::Little);
+    flags = IDFlags(*ptr);
+}
+
+//                 //
+// Private Methods //
+//                 //
+
+std::pair<Image, uint16_t> GIF::loadNextFrame(std::istream& is, const std::vector<RGBA>& globalColorTable)
+{
+    uint8_t identityByte;
+    is.read((char*)(&identityByte), 1);
+
+    // Make sure stream is still valid. If not, assume EOF has been reached
+    if (!is)
+    {
+        throw gif::EndOfStreamException("Failed to read first byte of block");
+    }
+
+    // Declarations
+    Image image;
+    uint16_t duration;
+
+    // Based on the type of this block, attempt to either load in a new
+    // frame or interpret an extension block
+    switch (identityByte)
+    {
+    case EXTENSION_INTRODUCER: {
+        // TODO
+        break;
+    } // case EXTENSION_INTRODUCER
+
+    case IMAGE_SEPARATOR: {
+        // Get the image descriptor
+        ImageDescriptor desc(is);
+
+        // Load raw image data
+        auto rawData = loadImageData(is);
+        // Parse raw data for LZW codes
+        auto lzwCodes = decompressLZW(rawData);
+
+        // Read color codes and create image
+        if (desc.flags.localTableFlag) {
+            std::vector<RGBA> localColorTable = loadColorTable(is, getColorTableSize(desc.flags.colorTableSize));
+            image = decodeImage(lzwCodes, localColorTable);
+        } else if (globalColorTable.size()) {
+            image = decodeImage(lzwCodes, globalColorTable);
+        } else {
+            throw gif::ColorTableException("Missing global and local color table!");
+        }
+        break;
+    } // case IMAGE_SEPARATOR
+
+    } // switch
+
+    return std::pair<Image, uint16_t>(image, duration);
+}
+
+std::vector<uint8_t> GIF::loadImageData(std::istream& is)
+{
+    std::vector<uint8_t> data;
+
+    // TODO
+
+    return data;
+}
+
+std::vector<uint16_t> GIF::decompressLZW(const std::vector<uint8_t>& data)
+{
+    std::vector<uint16_t> codes;
+
+    // TODO
+
+    return codes;
+}
+
+Image GIF::decodeImage(const std::vector<uint16_t>& codes, const std::vector<RGBA>& colorTable)
+{
+    Image image;
+
+    // TODO
+
+    return image;
+}
+
+std::vector<RGBA> GIF::loadColorTable(std::istream& is, uint8_t numColors)
+{
+    const unsigned bytesToRead = (unsigned)numColors * 3;
+    uint8_t* buf = new uint8_t[bytesToRead], *ptr = buf;
+    is.read((char*)buf, bytesToRead);
+    
+    std::vector<RGBA> table(numColors);
+    for (RGBA& color : table)
+    {
+        color = RGBA(
+            *ptr,
+            *(ptr + 1),
+            *(ptr + 2)
+        );
+
+        ptr += 3;
+    }
+
+    delete[] buf;
+    return table;
+}
+
+int GIF::getColorTableSize(int bitField)
+{
+    return std::pow(2, 1 + bitField);
+}
+
+namespace gif
+{
+
+//                    //
+// Helpful Exceptions //
+//                    //
+
+// GifLoadingException
+GifLoadingException::GifLoadingException(std::string error)
+    : std::runtime_error(error)
+{}
+
+std::ostream& operator<<(std::ostream& os, const GifLoadingException& e)
+{
+    return os << e.getErrorName() << ": " << e.what();
+}
+
+// EndOfStreamException
+EndOfStreamException::EndOfStreamException(std::string error)
+    : GifLoadingException(error)
+{}
+
+std::string EndOfStreamException::getErrorName() const
+{
+    return "[EndOfStreamException]";
+}
+
+// InterruptedStreamException
+InterruptedStreamException::InterruptedStreamException(std::string error)
+    : GifLoadingException(error)
+{}
+
+std::string InterruptedStreamException::getErrorName() const
+{
+    return "[InterruptedStreamException]";
+}
+
+// ColorTableException
+ColorTableException::ColorTableException(std::string error)
+    : GifLoadingException(error)
+{}
+
+std::string ColorTableException::getErrorName() const
+{
+    return "[ColorTableException]";
+}
+
+} // namespace gif
+
+} // namespace zmath
