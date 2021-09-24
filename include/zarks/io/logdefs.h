31418ca3256bc41b8df8b0bd5551564dd4077f1b
Isaac Brooks <iabrooks@mtu.edu>
2021-09-24 18:12:09 -0400

    Configurable end of log character

--- include/zarks/io/logdefs.h
+++ include/zarks/io/logdefs.h
@@ -2,8 +2,12 @@
 
 #include <iostream>
 
+#ifndef _ZARKS_LOG_NEWLINE
+    #define _ZARKS_LOG_NEWLINE std::endl;
+#endif // ifndef ZARKS_LOG_END_MESSAGE
+
 #ifdef ZARKS_DEBUG
-#define LOG_DEBUG( msg ) std::cout << "\033[1m[DEBUG]\033[0m " << msg << '\n';
+    #define LOG_DEBUG( msg ) std::cout << "\033[1m[DEBUG]\033[0m " << msg << _ZARKS_LOG_NEWLINE;
 #else
-#define LOG_DEBUG( msg )
-#endif
+    #define LOG_DEBUG( msg )
+#endif // ifndef ZARKS_DEBUG
