9947fe836a2ae1c0b691dacf08b3ab4bbb8c6374
Isaac Brooks <iabrooks@mtu.edu>
2021-10-22 16:18:30 -0400

    test_gif reduced to one basic test

--- test/test_gif.cpp
+++ test/test_gif.cpp
@@ -1,9 +1,11 @@
 #include <zarks/io/bitmath.h>
 #include <zarks/io/BitBuffer.h>
 #include <zarks/io/numerals.h>
+#include <zarks/io/logdefs.h>
 #include <zarks/image/GIF.h>
 
 #include <iostream>
+#include <string>
 
 using namespace zmath;
 
@@ -11,55 +13,18 @@ void testBitRange();
 void testBitBuffer();
 void testGIF();
 
-int main()
+int main(int argc, char** argv)
 {
-    testBitRange();
-    testBitBuffer();
-    testGIF();
+    if (argc != 2)
+    {
+        LOG_ERROR("test_gif should be run with: ./testgif infile.gif")
+        exit(1);
+    }
+
+    std::string infile = argv[1];
+    GIF gif(infile);
+    Image frame = gif.At(0);
+    frame.Save("frame.png");
 
     return 0;
-}
-
-void testBitRange()
-{
-    size_t val = BitRangeRev(0, 20);
-    size_t val_corr = 0xFFFFF00000000000;
-
-    if (val == val_corr) {
-        std::cout << "All good!\n";
-    } else {
-        std::cout << "Grrrr! " << val << " " << val_corr << "\n";
-    }
-
-    std::cout << BitRange(3, 6) << " " << BitRange(6, 3) << "\n";
-}
-
-void testBitBuffer()
-{
-    BitBuffer bbuf;
-
-    // 0b00000111
-    //        ^
-    bbuf << BitField(0b111, 3);
-    // 0b00001111
-    //      ^ 
-    bbuf << BitField(0b01, 2);
-    // 0b10001111
-    //   ^
-    bbuf << BitField(0b100, 3);
-
-    for (int i = 8; i >= 0; i--)
-    {
-        std::cout << bbuf[i];
-    }
-    std::cout << "\n";
-}
-
-void testGIF()
-{
-    GIF gif("giflib-logo.gif");
-
-    Image frame = gif.At(0);
-
-    frame.Save("frame.png");
 }
