9afc703e47ad79b3e306032f3edeb1184c3c991b
Isaac Brooks <iabrooks@mtu.edu>
2021-11-19 22:00:33 -0500

    Update test_gif

--- test/test_gif.cpp
+++ test/test_gif.cpp
@@ -9,24 +9,29 @@
 
 using namespace zmath;
 
-void testBitRange();
-void testBitBuffer();
-void testGIF();
-
 int main(int argc, char** argv)
 {
+    std::ios_base::sync_with_stdio(false);
+
     if (argc != 2)
     {
         LOG_ERROR("test_gif should be run with: ./testgif infile.gif")
         exit(1);
     }
 
+    // Load in GIF from file
     std::string infile = argv[1];
     GIF gif(infile);
     Image frame = gif.At(0);
     frame.Save("frame.png");
 
+    // Save just-loaded GIF in new file
+    LOG_INFO("Re-saving GIF as gif.gif");
     gif.Save("gif.gif", true);
+
+    // Ensure GIF was just saved correctly by loading it in
+    LOG_INFO("Re-loading GIF at gif.gif")
+    GIF reload("gif.gif");
 
     return 0;
 }
