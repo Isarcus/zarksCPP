2978f53f65760fd2152423bc0b03d4ef52b5d690
Isaac Brooks <iabrooks@mtu.edu>
2022-06-04 19:16:16 -0400

    Multithreaded Gaussian blur

--- include/zarks/math/GaussBlur.h
+++ include/zarks/math/GaussBlur.h
@@ -3,26 +3,75 @@
 #include <zarks/internal/Mat2D.h>
 #include <zarks/math/GaussField.h>
 
+#include <vector>
+#include <thread>
 #include <utility>
 
 namespace zmath
 {
 
-template<typename T, typename SUM_T = T>
-void GaussianBlur(Mat2D<T>& mat, int sigma, int devs = 2)
+template <typename T, typename SUM_T = T>
+void GaussianBlurChunk(const Mat2D<T>* mat,
+                       Mat2D<T>* blurred,
+                       VecInt min,
+                       VecInt max,
+                       const std::vector<std::pair<VecInt, double>>& points)
 {
-    GaussField gauss(sigma, 1.0);
+    for (int x = min.X; x < max.X; x++)
+    {
+        for (int y = min.Y; y < max.Y; y++)
+        {
+            VecInt blurCoord(x, y);
+
+            // Summation for this blurred pixel
+            SUM_T sum = SUM_T();
+            double weight = 0;
+            for (auto pair : points)
+            {
+                VecInt origCoord = blurCoord + pair.first;
+                if (blurred->ContainsCoord(origCoord))
+                {
+                    sum += (*mat)(origCoord) * pair.second;
+                    weight += pair.second;
+                }
+            }
+            (*blurred)(blurCoord) = sum / weight;
+        }
+    }
+}
+
+template<typename T, typename SUM_T = T>
+void GaussianBlur(Mat2D<T>& mat, int sigma, int devs = 2, int numThreads = std::thread::hardware_concurrency())
+{
     VecInt bounds = mat.Bounds();
     Mat2D<T> blurred(bounds);
 
     // Pre-compute weights for each point
-    const std::vector<std::pair<VecInt, double>> points = gauss.Points(sigma*devs);
+    const std::vector<std::pair<VecInt, double>> points = GaussField(sigma, 1.0).Points(sigma*devs);
 
-    // Perform Gaussian summation
-    for (int x = 0; x < bounds.X; x++)
+    if (numThreads > 1)
     {
-        for (int y = 0; y < bounds.Y; y++)
+        // Split passed matrix into regions to deal with per thread
+        std::vector<std::thread> threads(numThreads);
+        for (int i = 0; i < numThreads; i++)
         {
+            threads[i] = std::thread(
+                GaussianBlurChunk<T, SUM_T>,
+                &mat,
+                &blurred,
+                Vec(bounds.X*(double(i) / numThreads), 0),
+                Vec(bounds.X*(double(i+1) / numThreads), bounds.Y),
+                points
+            );
+        }
+        for (int i = 0; i < numThreads; i++)
+        {
+            threads[i].join();
+        }
+    }
+    else
+    {
+        blurred.Apply([&](int x, int y){
             VecInt blurCoord(x, y);
 
             // Summation for this blurred pixel
@@ -37,11 +86,11 @@ void GaussianBlur(Mat2D<T>& mat, int sigma, int devs = 2)
                     weight += pair.second;
                 }
             }
-            blurred(blurCoord) = sum / weight;
-        }
+            return sum / weight;
+        });
     }
 
     mat = blurred;
 }
 
-}
+} // namespace zmath
